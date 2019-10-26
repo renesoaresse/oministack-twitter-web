51fcb2d77ba5f0145300d835fd1630dd5ed7a10e
Renê Soares <reenerochase@gmail.com>
2019-10-25 23:24:18 -0300

    feat(screen) WIP:Creation of tweet send function.

--- src/pages/TimeLine.js
+++ src/pages/TimeLine.js
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
 
+import logo from "../twitter.svg";
 import "./Timeline.css";
 
 export default class TimeLine extends Component {
+  state = {
+    newTweet: ""
+  };
+
+  handleNewTweet = e => {
+    if (e.keyCode !== 13) return;
+
+    const { newTweet: content } = this.state;
+    const author = localStorage.getItem("@Twitter:username");
+
+    console.log(content, author);
+
+    this.setState({ newTweet: "" });
+  };
+
+  handleTextChange = e => {
+    this.setState({ newTweet: e.target.value });
+  };
+
   render() {
-    return <h1>TimeLine</h1>;
+    const { newTweet } = this.state;
+
+    return (
+      <div className="timeline-wrapper">
+        <img height={24} src={logo} alt="Twitter" />
+
+        <form>
+          <textarea
+            value={newTweet}
+            onChange={this.handleTextChange}
+            onKeyDown={this.handleNewTweet}
+            placeholder="O que está pensando?"
+          />
+        </form>
+      </div>
+    );
   }
 }
