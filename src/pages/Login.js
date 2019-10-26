f37d22f1052979123af6de37f60f91cd34d378e7
Renê Soares <reenerochase@gmail.com>
2019-10-25 22:51:18 -0300

    feat(screen): Created the function to change the state.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,12 +4,25 @@ import logo from "../twitter.svg";
 import "./Login.css";
 
 export default class Login extends Component {
+  state = {
+    username: ""
+  };
+
+  handleInputChange = e => {
+    this.setState({ username: e.target.value });
+  };
+
   render() {
+    const { username } = this.state;
     return (
       <div className="login-wrapper">
         <img src={logo} alt="Twitter" />
         <form>
-          <input placeholder="Nome de usuário" />
+          <input
+            value={username}
+            onChange={this.handleInputChange}
+            placeholder="Nome de usuário"
+          />
           <button type="submit">Entrar</button>
         </form>
       </div>
